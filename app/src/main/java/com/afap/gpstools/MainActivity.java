4ac397c287c8bf673a776a2b88499f242a4f5858
AFAP <814237762@qq.com>
2017-06-02 16:46:53 +0800

    Temp

--- app/src/main/java/com/afap/gpstools/MainActivity.java
+++ app/src/main/java/com/afap/gpstools/MainActivity.java
@@ -1,12 +1,23 @@
 package com.afap.gpstools;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.GnssStatus;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -24,8 +35,11 @@ import android.widget.TextView;
 import com.afap.gpstools.utils.LogUtil;
 import com.afap.gpstools.widget.CompassView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, SensorEventListener {
@@ -74,7 +88,92 @@ public class MainActivity extends AppCompatActivity
         mfSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
 
+        final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                // TODO: Consider calling
+//                //    ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                //                                          int[] grantResults)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//                return;
+//            }
+//            LogUtil.d(TAG, "GnssStatus");
+//            locationManager.registerGnssStatusCallback(new GnssStatus.Callback() {
+//                @Override
+//                public void onStarted() {
+//                    super.onStarted();
+//                    LogUtil.d(TAG, "GnssStatus：onStarted");
+//                }
+//
+//
+//                @Override
+//                public void onSatelliteStatusChanged(GnssStatus status) {
+//                    super.onSatelliteStatusChanged(status);
+//
+////                    int num = status.getSatelliteCount();
+////                    LogUtil.d(TAG, "数量：" + num);
+////                    for (int i = 0; i < num; i++) {
+////                        String info = "卫星信息：\n信噪：" + status.getCn0DbHz(i);
+////                        info += "\n高度：" + status.getElevationDegrees(i);
+////                        LogUtil.d(TAG, info);
+////                    }
+//
+//
+//                }
+//
+//
+//            });
+//        } else {
+        locationManager.addGpsStatusListener(new GpsStatus.Listener() {
+            @Override
+            public void onGpsStatusChanged(int event) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+
+                GpsStatus status = locationManager.getGpsStatus(null);
+
+
+                StringBuilder sb2 = new StringBuilder("");
+                if (status == null) {
+                    sb2.append("搜索到卫星个数：" + 0);
+                } else if (event == GpsStatus.GPS_EVENT_SATELLITE_STATUS) {
+                    int maxSatellites = status.getMaxSatellites();
+                    Iterator<GpsSatellite> it = status.getSatellites().iterator();
+                    numSatelliteList.clear();
+                    int count = 0;
+                    while (it.hasNext() && count <= maxSatellites) {
+                        GpsSatellite s = it.next();
+                        numSatelliteList.add(s);
+
+
+
+                        count++;
+                    }
+                    sb2.append("搜索到卫星个数：" + numSatelliteList.size());
+                    compassView.updateGpsSatellite(numSatelliteList);
+                }
+
+                LogUtil.d(TAG, sb2.toString());
+
+            }
+        });
+//        }
+
+
     }
+
+    private List<GpsSatellite> numSatelliteList = new ArrayList<GpsSatellite>(); // 卫星信号
 
     @Override
     protected void onResume() {
@@ -154,9 +253,26 @@ public class MainActivity extends AppCompatActivity
     private float[] magneticFieldValues = new float[3];
 
 
+    /**
+     * ********╲*******Y️
+     * *********╲*****⬆️
+     * **********╲****|**********
+     * ***********╲***|**********
+     * ************╲**|**********
+     * *************╲*|**********
+     * **************╲|**********
+     * ------------------------------> X （手机宽度 左--右）
+     * ***************|╲*********
+     * ***************|*╲********
+     * ***************|**╲*******
+     * ***************|***╲******
+     * ***************|****╲*****
+     * ***************|*****╲****
+     * ***************|******╲***
+     * ***************|*******Z**（手机平放时朝上方向）
+     */
     @Override
     public void onSensorChanged(SensorEvent event) {
-        LogUtil.d(TAG, "onSensorChanged----->");
 
         if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
             magneticFieldValues = event.values;
@@ -179,7 +295,7 @@ public class MainActivity extends AppCompatActivity
         info += "\n[X]Pitch:" + values[1];
         info += "\n[Y]Pitch:" + values[2];
 
-        LogUtil.i(TAG, info);
+//        LogUtil.i(TAG, info);
 
 //        if (values[0] >= -5 && values[0] < 5) {
 //            tv_info_detail.setText("正北");
@@ -229,7 +345,7 @@ public class MainActivity extends AppCompatActivity
 
             }
         });
-        compassView.startAnimation(animate);
+//        compassView.startAnimation(animate);
 
 
     }
@@ -238,4 +354,42 @@ public class MainActivity extends AppCompatActivity
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         LogUtil.i(TAG, "onAccuracyChanged:" + sensor.getName() + ",accuracy=" + accuracy);
     }
+
+
+//    /**
+//     * 卫星状态监听器
+//     */
+//    private List<GpsSatellite> numSatelliteList = new ArrayList<GpsSatellite>(); // 卫星信号
+//
+//    private final GpsStatus.Listener statusListener = new GpsStatus.Listener() {
+//        public void onGpsStatusChanged(int event) { // GPS状态变化时的回调，如卫星数
+//
+//            GpsStatus status = locationManager.getGpsStatus(null); //取当前状态
+//            String satelliteInfo = updateGpsStatus(event, status);
+//            tv_satellites.setText(null);
+//            tv_satellites.setText(satelliteInfo);
+//        }
+//    };
+//
+//    private String updateGpsStatus(int event, GpsStatus status) {
+//        StringBuilder sb2 = new StringBuilder("");
+//        if (status == null) {
+//            sb2.append("搜索到卫星个数：" + 0);
+//        } else if (event == GpsStatus.GPS_EVENT_SATELLITE_STATUS) {
+//            int maxSatellites = status.getMaxSatellites();
+//            Iterator<GpsSatellite> it = status.getSatellites().iterator();
+//            numSatelliteList.clear();
+//            int count = 0;
+//            while (it.hasNext() && count <= maxSatellites) {
+//                GpsSatellite s = it.next();
+//                numSatelliteList.add(s);
+//                count++;
+//            }
+//            sb2.append("搜索到卫星个数：" + numSatelliteList.size());
+//        }
+//
+//        return sb2.toString();
+//    }
+
+
 }
--- app/src/main/java/com/afap/gpstools/widget/CompassView.java
+++ app/src/main/java/com/afap/gpstools/widget/CompassView.java
@@ -3,14 +3,19 @@ package com.afap.gpstools.widget;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.location.GpsSatellite;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
 
 import com.afap.gpstools.R;
+import com.afap.gpstools.utils.LogUtil;
+
+import java.util.List;
 
 /**
  * 罗盘控件
@@ -18,10 +23,16 @@ import com.afap.gpstools.R;
 
 public class CompassView extends View {
     private final static int SIZI_TEXT_SP = 16;
+    private final static int SIZI_TEXT_DEGREE_SP = 10;
+
     private final static int MARGIN_CIRCLE_DP = 5;
     private final static int POINT_O_R = 3;
     private final static int SIZE_BORDER = 1;
     private final static float SIZE_LINE = 0.5f;
+
+
+    private Canvas mCanvas;
+    private List<GpsSatellite> gpsSatellites; // 卫星列表
 
 
     public CompassView(Context context) {
@@ -35,6 +46,7 @@ public class CompassView extends View {
     public CompassView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
+
 
     private int getMySize(int defaultSize, int measureSpec) {
         int mySize = defaultSize;
@@ -71,14 +83,18 @@ public class CompassView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Context context = getContext();
+        LogUtil.d("CompassView", "onDraw");
 
-        String east = context.getString(R.string.east);
-        String west = context.getString(R.string.west);
-        String south = context.getString(R.string.south);
-        String north = context.getString(R.string.north);
+
+        Context context = getContext();
+        mCanvas = canvas;
+
+        String[] directions = context.getResources().getStringArray(R.array.directions);
+
 
         float size_text = sp2px(context, SIZI_TEXT_SP);
+        float size_text_degree = sp2px(context, SIZI_TEXT_DEGREE_SP);
+
         float margin_circle = dp2px(context, MARGIN_CIRCLE_DP);
         float point_o_r = dp2px(context, POINT_O_R);
         float size_border = dp2px(context, SIZE_BORDER);
@@ -88,31 +104,24 @@ public class CompassView extends View {
 
         float o_x = getLeft() + w / 2; // 圆心-X
         float o_y = getTop() + w / 2; // 圆心-Y
-        float r = w / 2 - size_text - margin_circle;
+        float r0 = w / 2 - size_text - margin_circle;
+        float r = r0 - size_text_degree;
 
 
-        Paint fontPaint = new Paint();
-        fontPaint.setColor(Color.BLACK);
-//        fontPaint.setTypeface(Typeface.DEFAULT_BOLD);
-        fontPaint.setAntiAlias(true);//去除锯齿
-        fontPaint.setTextSize(size_text);
+        canvas.translate(o_x, o_y); // 移动原点到圆心位置
 
-        canvas.translate(o_x, o_y);
-
-        canvas.drawText(north, -fontPaint.measureText(north) / 2, -o_y + size_text, fontPaint);
-        canvas.rotate(90);
-        canvas.drawText(east, -fontPaint.measureText(east) / 2, -o_y + size_text, fontPaint);
-        canvas.rotate(90);
-        canvas.drawText(south, -fontPaint.measureText(south) / 2, -o_y + size_text, fontPaint);
-        canvas.rotate(90);
-        canvas.drawText(west, -fontPaint.measureText(west) / 2, -o_y + size_text, fontPaint);
 
         Paint paint = new Paint();
         paint.setColor(Color.BLACK);
-        paint.setAntiAlias(true);//去除锯齿
+        paint.setAntiAlias(true); //去除锯齿
         canvas.drawCircle(0, 0, point_o_r, paint);
-        paint.setStyle(Paint.Style.STROKE);//设置空心
+        paint.setStyle(Paint.Style.STROKE); //设置空心
         paint.setStrokeWidth(size_border);
+        // 最外围的圆圈
+        canvas.drawCircle(0, 0, r0, paint);
+        //内侧圆圈使用虚线显示
+        paint.setPathEffect(new DashPathEffect(new float[]{16, 8}, 0));
+
         canvas.drawCircle(0, 0, r, paint);
         canvas.drawCircle(0, 0, r * 0.66f, paint);
         canvas.drawCircle(0, 0, r * 0.33f, paint);
@@ -121,7 +130,64 @@ public class CompassView extends View {
         canvas.drawLine(-r, 0, r, 0, paint);
         canvas.drawLine(0, -r, 0, r, paint);
 
+        // 旋转至正北方向，开始绘制角度虚线和角度值
+//        canvas.rotate(-90);
+        paint.setPathEffect(new DashPathEffect(new float[]{6, 3}, 0));
+
+        Paint fontPaint = new Paint();
+        fontPaint.setColor(Color.BLACK);
+        fontPaint.setAntiAlias(true);//去除锯齿
+
+        for (int i = 0; i < 12; i++) {
+            if (i % 3 == 0) {
+                fontPaint.setTextSize(size_text);
+                String direction = directions[i / 3];
+                canvas.drawText(direction, -fontPaint.measureText(direction) / 2, -o_y + size_text, fontPaint);
+            }
+            fontPaint.setTextSize(size_text_degree);
+            canvas.drawLine(0, 0, r, 0, paint);
+            String degree = "" + 30 * i;
+            canvas.drawText(degree, -fontPaint.measureText(degree) / 2, -r, fontPaint);
+
+            canvas.rotate(30 * 1);
+        }
+
+        canvas.save();
+
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL);
+
+        if (gpsSatellites == null || gpsSatellites.size() == 0) {
+            return;
+        }
+
+        for (GpsSatellite gpsSatellite : gpsSatellites) {
+            int pnr = (int) gpsSatellite.getPrn();
+            float azimuth = gpsSatellite.getAzimuth();
+            float elevation = gpsSatellite.getElevation();
+            if (azimuth == 0 && elevation == 0) {
+                continue;
+            }
+            float x = (float) (Math.cos(azimuth) * elevation * r / 90);
+            float y = (float) (Math.sin(azimuth) * elevation * r / 90);
+
+            mCanvas.drawCircle(x, y, point_o_r, paint);
+
+
+            LogUtil.d("cc", "snr:" + pnr + "\nelevation:" + elevation);
+
+        }
+
     }
+
+    public void updateGpsSatellite(List<GpsSatellite> satellites) {
+        gpsSatellites = satellites;
+
+        postInvalidate();
+
+
+    }
+
 
     private float dp2px(Context context, float dp) {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
